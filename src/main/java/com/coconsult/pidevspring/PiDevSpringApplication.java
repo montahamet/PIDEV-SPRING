b5d18bf90b7056d254e7728330d503a2a96269e4
montahamet <montaha.metjaouel@esprit.tn>
2024-02-13 16:33:10 +0100

    first commmit

---
+++ src/main/java/com/coconsult/pidevspring/PiDevSpringApplication.java
@@ -0,0 +1,13 @@
+package com.coconsult.pidevspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiDevSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiDevSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coconsult/pidevspring/PiDevSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coconsult.pidevspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiDevSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
