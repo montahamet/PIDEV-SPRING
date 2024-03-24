401ca35cb63c2b79dd23259390926c15c2829916
user <haifabensalah81@gmail.com>
2024-03-24 23:24:08 +0100

    modification au niveau  du service update event

--- src/main/java/com/coconsult/pidevspring/Services/TrainingSession/EventService.java
+++ src/main/java/com/coconsult/pidevspring/Services/TrainingSession/EventService.java
@@ -6,6 +6,7 @@ import com.coconsult.pidevspring.DAO.Entities.RegistrationEvent;
 import com.coconsult.pidevspring.DAO.Entities.User;
 import com.coconsult.pidevspring.DAO.Repository.TrainingSession.EventRepository;
 import com.coconsult.pidevspring.DAO.Repository.TrainingSession.RegistrationEventRepository;
+import org.springdoc.api.OpenApiResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -38,10 +39,26 @@ public class EventService implements IEventService {
         return eventRepository.save(event);
     }
 
-    @Override
-    public Event UpdateEvent(Event event) {
-        return eventRepository.save(event);
-    }
+//    @Override
+//    public Event UpdateEvent(Event event) {
+//        return eventRepository.save(event);
+//    }
+@Override
+public Event UpdateEvent(Event event) {
+    // Vérifier si l'événement existe avant de le mettre à jour
+    return eventRepository.findById(event.getEvent_id())
+            .map(existingEvent -> {
+                // Mettez à jour ici les champs de l'existingEvent avec ceux de l'event
+                existingEvent.setEvent_name(event.getEvent_name());
+                existingEvent.setEvent_description(event.getEvent_description());
+                existingEvent.setEvent_date(event.getEvent_date());
+                existingEvent.setPlace(event.getPlace());
+                // Ajoutez d'autres champs à mettre à jour si nécessaire
+                return eventRepository.save(existingEvent);
+            })
+            .orElseThrow(() -> new OpenApiResourceNotFoundException("Event not found with id " + event.getEvent_id()));
+}
+
 
     @Override
     public void deleteEventById(Long eventId) {
